e1ea7695716608a604bbc76d35b9254436e40584
jierlausson <jierlausson@hotmail.com>
2022-01-04 00:34:41 -0300

    Alter lista-beer

--- resources/views/livewire/lista-beer.blade.php
+++ resources/views/livewire/lista-beer.blade.php
@@ -3,20 +3,20 @@
     <input type="text" wire:model="item">
 
     @if ($action == 'Cadastrar')
-        <button wire:click="add">Adicionar</button>
+        <button wire:click="add" class="btn btn-outline-primary btn-sm">Adicionar</button>
     @else
-        <button wire:click="update">Atualizar</button>
+        <button wire:click="update" class="btn btn-outline-primary btn-sm">Atualizar</button>
     @endif
 
-    <button wire:click="resetList">Apagar Lista</button>
+    <button wire:click="resetList" class="btn btn-outline-warning btn-sm">Apagar Lista</button>
 
     <p>
         <ul>
             @foreach ($list as $key => $nome)
                 <li>
-                    <span>{{ $nome }} - </span>
-                    <button wire:click="edit({{ $key }})">Editar</button>
-                    <button wire:click="delete({{ $key }})">X</button>
+                    <span>{{ $nome }}</span>
+                    <button wire:click="edit({{ $key }})" class="btn btn-outline-primary btn-sm mb-2">Editar</button>
+                    <button wire:click="delete({{ $key }})" class="btn btn-outline-danger btn-sm mb-2">X</button>
                 </li>
             @endforeach
         </ul>
