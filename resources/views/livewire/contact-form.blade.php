20d3779d18274032b1caddb3589c09e50c7b87a1
jierlausson <jierlausson@hotmail.com>
2022-01-04 23:58:00 -0300

    Contact-form input's

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -1,3 +1,28 @@
-<div>
-    Teste!
+<div class="form4 top">
+    <div class="container">
+      <div class="row d-flex justfy-content-center">
+        <div class="col-md-7">
+          <div class="form-bg">
+            <form class="form">
+              <div class="form-group">
+                <label class="sr-only">Name</label>
+                <input  type="text"
+                        name="name"
+                        class="form-control"
+                        placeholder="Seu nome">
+                <!-- vadation-->
+              </div>
+              <div class="form-group">
+                <label class="sr-only">Email</label>
+                <input  type="email"
+                        name="email"
+                        class="form-control"
+                        placeholder="Email">
+                <!--validation-->
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
 </div>
